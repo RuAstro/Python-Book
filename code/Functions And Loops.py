298f50c1e4d3bef2a5c6f4ab9098465811eeffd2
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-03-08 09:52:04 +0200

    Initial work

---
+++ code/Functions And Loops.py
@@ -0,0 +1,25 @@
+def multiply(x, y):
+    product = x * y
+    return product
+
+    num = multiply(2, 4)
+    print(num)
+
+
+def greet(RJ):
+    print(f"Hello, {name}!" )
+
+def cube(num):
+    """return the cube of the input number."""
+    cube_num = num**3
+    return cube_num
+
+print(f"0 cubed is {cube(0)}")
+print(f"2 cubed is {cube(2)}")
+
+def convert_cel_to_far(temp_cel):
+    """Return the Celsius temperature temp_cel converted to Fahrenheit."""
+    temp_far = temp_cel *(9 / 5) + 32
+    return temp_far
+    
+
