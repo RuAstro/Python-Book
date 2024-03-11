4b77ba4d69a0b5cb932e0dbcbf22a923b4facda9
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-03-11 10:43:07 +0200

    edit out the spaces between .2f

--- code/invest.py
+++ code/invest.py
@@ -2,7 +2,7 @@ def invest(amount, rate, years):
     """Display year on year growth of an initial investment"""
     for year in range(1, years + 1):
         amount = amount * (1 + rate)
-        print(f"year {year}: ${amount:, .2f} ")
+        print(f"year {year}: ${amount:,.2f} ")
 
 amount = float(input("Enter a principal amount: "))
 rate = float(input("Enter an return rate: "))
