e0687f6f3c8767537a3493011f15a8e4a171c7fa
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-03-12 10:38:28 +0200

    Fixed spelling on words

--- code/control_flow_of_program.py
+++ code/control_flow_of_program.py
@@ -1,19 +1,19 @@
-my_input = input("Type in a word: ")
+#my_input = input("Type in a word: ")
 
-if len(my_input) < 5:
-    print("Your input is less than 5 characters long")
-elif len(my_input) > 5:
-    print("Your input is greather than 5 characters long")
+#if len(my_input) < 5:
+ #   print("Your input is less than 5 characters long")
+#elif len(my_input) > 5:
+ #   print("Your input is greater than 5 characters long")
 
-else:
-    print("Your input is 5 characters long")
+#else:
+#    print("Your input is 5 characters long")
     
     
     
 print("I'm thinking of a number between 1 and 10. Guess which one. ")
-my_geuss = input("What number are you Geussing? ")
+my_guess = input("What number are you Guessing? ")
 
-if my_geuss == "3":
+if my_guess == "3":
     print("you win!")
     
 else:
