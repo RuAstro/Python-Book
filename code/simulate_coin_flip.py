0b78dfd8d73029faf035233c342723072237266b
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-03-13 09:10:28 +0200

    coin flip program

---
+++ code/simulate_coin_flip.py
@@ -0,0 +1,22 @@
+import random
+
+
+def coin_flip():
+    """Randomly returns 'heads' and 'tails'. """
+    if random.radiant(0, 1) == 0:
+        return "heads"
+    else:
+        return "tails"
+    
+heads_tally = 0
+tails_tally = 0
+
+for trail in range(10_000):
+    if coin_flip() == "heads":
+        heads_tally = heads_tally + 1
+        
+    else:
+        tails_tally = tails_tally + 1
+        
+ratio = heads_tally / tails_tally
+print(f"The ratio of heads to tails is {ratio}")
