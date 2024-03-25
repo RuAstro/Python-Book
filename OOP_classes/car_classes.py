92b2a9e954e993d9b73d6cdb1505aee4fe2be6f1
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-03-25 15:23:21 +0200

    car class exercise

---
+++ OOP_classes/car_classes.py
@@ -0,0 +1,17 @@
+
+
+class Car:
+
+    def __init__(self, name, color, km, drive):
+        self.name = name
+        self.color = color
+        self.km = km
+        self.drive = drive
+        
+    blue_car = (20_000)
+    red_car = (30_000)
+    km = ()
+    
+    print(f"The blue car has {blue_car} km ")
+    print(f"And the red car has {red_car} km")
+    
