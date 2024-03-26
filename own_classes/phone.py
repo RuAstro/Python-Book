f3ce82b43d3faed96d08c0d9c22e2c9931d14eea
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-03-26 15:39:51 +0200

    added notifi as instance method

--- own_classes/phone.py
+++ own_classes/phone.py
@@ -1,3 +1,16 @@
 
 class Phone:
-    pass
+    
+    def __init__(self, name, model, length, weight, color):
+        self.name = name
+        self.model = model
+        self.length = length
+        self.weight = weight
+        self.color = color
+        
+    def Call(self):
+        print("Ring...")
+        
+    def Notification(self):
+        print("Ting...")
+    
