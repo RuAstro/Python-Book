0cb1fbee97b969429d69a50787ba8fc05892a2b1
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-02 12:40:54 +0200

    added super()

--- own_classes/charge_seating.py
+++ own_classes/charge_seating.py
@@ -9,7 +9,7 @@ class Vehicle:
         
 class Bus(Vehicle):
     def fare(self):
-        amount = fare()
+        amount = super().fare()
         amount = amount * 10 / 100          #amount = total fare + (multiply) 10% of the total fare.
         return amount
 
