0b13ef3f49dae9b028e232e350bde8b417d7fc9b
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-02 10:44:22 +0200

    def the capacity for the bus

--- own_classes/max_speed_and_mileage.py
+++ own_classes/max_speed_and_mileage.py
@@ -7,8 +7,13 @@ class Car:
 # CarX = Car(200, 28)
 # print(CarX.max_speed, CarX.mileage)
 
+    def seating_capacity(self, capacity):
+        return  f"The seating capacity of a {self.name} is {capacity} passengers."
+
+
 class Bus(Car):
-    pass
+    def seating_capacity(self, space = 50):
+        return super().seating_capacity()
 
 Car_Name = Bus("Volvo", 100, 15)
 print("Car Name: ",Car_Name.name , "Speed: ",Car_Name.max_speed, "Mileage: ",Car_Name.mileage)
