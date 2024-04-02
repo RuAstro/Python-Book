01d648b4849c8d5891b27cc0b0aa2fea4872c62d
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-02 10:23:37 +0200

    print out car, class for buss that inherit

--- own_classes/max_speed_and_mileage.py
+++ own_classes/max_speed_and_mileage.py
@@ -4,5 +4,11 @@ class Car:
         self.mileage = mileage
         self.name = name
         
-CarX = Car(200, 28)
-print(CarX.max_speed, CarX.mileage)
+# CarX = Car(200, 28)
+# print(CarX.max_speed, CarX.mileage)
+
+class Bus(Car):
+    pass
+
+Car_Name = Bus("Volvo", 100, 15)
+print("Car Name: ",Car_Name.name , "Speed: ",Car_Name.max_speed, "Mileage: ",Car_Name.mileage)
