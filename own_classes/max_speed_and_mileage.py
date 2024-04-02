9f82502bed7432b491f950cbdd293b5546d85a19
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-02 10:09:10 +0200

    added name of car

--- own_classes/max_speed_and_mileage.py
+++ own_classes/max_speed_and_mileage.py
@@ -1,7 +1,8 @@
 class Car:
-    def __init__(self, max_speed, mileage):
+    def __init__(self, name, max_speed, mileage):
         self.max_speed = max_speed
         self.mileage = mileage
+        self.name = name
         
 CarX = Car(200, 28)
 print(CarX.max_speed, CarX.mileage)
