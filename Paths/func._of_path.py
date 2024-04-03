4a04c3497e488f797967bd1014d45a9d3f49a6d8
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-03 12:29:13 +0200

    List all items in the directory

--- Paths/func._of_path.py
+++ Paths/func._of_path.py
@@ -8,3 +8,8 @@ print("Exists", path.exists())
 
 #check if its a directory
 print("Is directory:", path.is_dir())
+
+#List all items in the directory
+print("Items in directory:")
+for item in path.iterdir():
+    print(item)
