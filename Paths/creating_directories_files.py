12c07f88ef599a43105222f77b001d24c934970d
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-04 10:33:26 +0200

    created new directories

---
+++ Paths/creating_directories_files.py
@@ -0,0 +1,12 @@
+#After importing the Path class, you create a new path to a directory
+#called new_directory/ in your home folder and assign this path to the
+#new_dir variable. Then you use .mkdir() to create the new directory.
+ 
+
+from pathlib import Path
+new_dir = Path.home() / "new_directory"
+new_dir.mkdir()
+
+print(new_dir.exists())
+
+print(new_dir.is_dir())
