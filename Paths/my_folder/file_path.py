45d8c0e769d8a38529fd493124491a370998e659
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-04 09:07:32 +0200

    exercise of paths

---
+++ Paths/my_folder/file_path.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+file_path = Path.home()  #Path.home() class method creates a Path object representing the home directory.
+
+print(file_path.exists())
+
+print(file_path = "my_folder"/ "my_file.txt")
