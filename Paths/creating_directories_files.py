a5e5b062b841e8568d3e34aa39168b668aa9de8f
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-05 09:53:23 +0200

    deleting folder, you must first delete everything inside the folder before deleting

--- Paths/creating_directories_files.py
+++ Paths/creating_directories_files.py
@@ -76,9 +76,9 @@ for path in paths:
 
 
 #Renames the folder_c...
-# source = new_dir / "folder_c"
-# destination = new_dir / "folder_d"
-# source.replace(destination)
+source = new_dir / "folder_c"
+destination = new_dir / "folder_d"
+source.replace(destination)
 
 
 #Delete a file...
@@ -89,3 +89,9 @@ file_path.exists()
 
 #ignore the exception...
 file_path.unlink(missing_ok = True)
+
+#for deleting a folder, you should first delete all files inside that folder
+for path in folder_d.iterdir():
+    path.unlink()
+    
+    folder_d.rmdir()
