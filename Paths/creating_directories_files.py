ec9b6f13d2d570840273fd57f252181bb58a0d8f
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-05 08:38:03 +0200

    matches file1 and 2.txt and folder_c/file2.txt

--- Paths/creating_directories_files.py
+++ Paths/creating_directories_files.py
@@ -60,4 +60,5 @@ for path in paths:
 #list(new_dir.glob("program?.py"))
 #list(new_dir.glob("?older_?"))
 #list(new_dir.glob("*1.??"))
-list(new_dir.glob("program[13].py"))
+#list(new_dir.glob("program[13].py"))
+list(new_dir.glob("**/*.txt"))
