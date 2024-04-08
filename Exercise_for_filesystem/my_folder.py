a5fb9b3d6bf21ec9755eb022c0c414e90af41cd9
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-08 10:40:46 +0200

    moved image1 to new dir. in my folder

--- Exercise_for_filesystem/my_folder.py
+++ Exercise_for_filesystem/my_folder.py
@@ -9,5 +9,9 @@ file_path.touch()
 file_path = new_dir / "file2.txt"
 file_path.touch()
 
-image1 = new_dir / "image1.jpg"
+image1 = new_dir / "image1.png"
 image1.touch()
+
+folder_images = new_dir / "images"
+#folder_images.mkdir()
+image1.replace(folder_images / "image1.png")
