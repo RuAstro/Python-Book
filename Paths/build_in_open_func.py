869395806aaf242fbe6a627d530e78b979ad9684
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-08 14:58:44 +0200

    uses the with statement

--- Paths/build_in_open_func.py
+++ Paths/build_in_open_func.py
@@ -2,3 +2,6 @@ file_path = "C:/Users/User/hello.txt"
 file = open(file_path, mode="r", encoding = "utf-8")
 file
 file.close()
+
+#the With statement...
+with open(file_path, mode = "r", encoding = "utf-8") as file:
