b5a542f42353642aa4fd9685af3283b303402883
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-08 10:13:42 +0200

    added 3 files to directory

--- Exercise_for_filesystem/my_folder.py
+++ Exercise_for_filesystem/my_folder.py
@@ -2,3 +2,12 @@
 from pathlib import Path
 new_dir = Path.home() / "my_folder"
 #new_dir.mkdir()
+
+file_path = new_dir / "file1.txt"
+file_path.touch()
+
+file_path = new_dir / "file2.txt"
+file_path.touch()
+
+image1 = new_dir / "image1.jpg"
+image1.touch()
