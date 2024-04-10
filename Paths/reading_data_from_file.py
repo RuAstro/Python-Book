657713dfa3926953141b0580ad54f8154a103acb
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-10 10:45:55 +0200

    opening the reading from the file

--- Paths/reading_data_from_file.py
+++ Paths/reading_data_from_file.py
@@ -23,3 +23,8 @@ print(text)
 
 with path.open(mode = "a", encoding = "utf-8") as file:
     file.write("\nHello")
+    
+with path.open(mode = "r", encoding = "utf-8") as file:
+    text = file.read()
+    
+print(text)
