1da2cec785214961720b7af90e06e1c5319a66ca
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-10 10:40:41 +0200

    verify that the file contains text

--- Paths/reading_data_from_file.py
+++ Paths/reading_data_from_file.py
@@ -13,3 +13,9 @@ with path.open(mode = "r", encoding = "utf-8") as file:
 
 with path.open(mode = "w", encoding = "utf-8") as file:
     file.write("Hi there!")
+    
+    
+#with path.open(mode = "r", encoding = "utf-8") as file:
+#    text = file.read()
+    
+print(text)
