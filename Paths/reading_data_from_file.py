7bf7c139eb30451ecc57560471c932e60e4c66dc
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-10 11:10:59 +0200

    added write lines

--- Paths/reading_data_from_file.py
+++ Paths/reading_data_from_file.py
@@ -28,3 +28,13 @@ with path.open(mode = "r", encoding = "utf-8") as file:
     text = file.read()
     
 print(text)
+
+
+lines_of_text = [
+    "Hello from line1\n",
+    "Hello from line2\n",
+    "Hello from line3\n", 
+]
+
+with path.open(mode = "w", encoding = "utf-8") as file:
+    file.writelines(lines_of_text)
