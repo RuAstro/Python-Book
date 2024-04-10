05c7fda4eea28ed94537a7ccdb6d2487b7873be5
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-10 11:45:43 +0200

    put each word on separate line

--- Paths/reading_and_writing_files.py
+++ Paths/reading_and_writing_files.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+starships = ("Discovery", "\nEnterprise", "\nDefiant", "\nVoyager")
+
 file_path = Path.home() / "starships.txt"
 with file_path.open(mode = "w", encoding = "utf-8")as file:
     file.writelines(starships)
