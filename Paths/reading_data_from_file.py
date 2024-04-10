f58d41485e0d66172ff2f54aa9117d097bd7b835
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-10 10:06:43 +0200

    added "as file" so that file.read get visible

--- Paths/reading_data_from_file.py
+++ Paths/reading_data_from_file.py
@@ -1,4 +1,5 @@
 from pathlib import Path
 path = Path.home() / "hello.txt"
-with path.open(mode = "r", encoding = "utf-8"):
+with path.open(mode = "r", encoding = "utf-8") as file:
     text = file.read()
+        
