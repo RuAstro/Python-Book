ade40c6d8f7fd2e2d9347824237e20fa952576b5
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-10 11:54:09 +0200

    printed each line of text in file

--- Paths/reading_and_writing_files.py
+++ Paths/reading_and_writing_files.py
@@ -5,3 +5,8 @@ starships = ("Discovery", "\nEnterprise", "\nDefiant", "\nVoyager")
 file_path = Path.home() / "starships.txt"
 with file_path.open(mode = "w", encoding = "utf-8")as file:
     file.writelines(starships)
+
+
+with file_path.open(mode = "r", encoding = "utf-8") as file:
+    for starship in file.readlines():
+        print(starship, end="")
