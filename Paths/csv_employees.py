07944fa6a705bb171080ca1f7dd4a8dfe9e84da6
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-11 08:53:25 +0200

    added row in reader and print row, and close file()

--- Paths/csv_employees.py
+++ Paths/csv_employees.py
@@ -1,16 +1,21 @@
 import csv
 from pathlib import Path
 
-employees = [
+'''employees = [
     ["name","department","salary"]
     ["Lee","Operations",75000.00]
     ["Jane","Engineering",85000.00]
     ["Diego","Sales",80000.00]
-]
+]'''
 
 file_path = Path.home() / "employees.csv"
 file = file_path.open(mode = "r", encoding = "utf-8", newline="")
 reader = csv.DictReader(file)
 
+reader.fieldnames
 
-
+for row in reader:
+    print(row)
+    
+    
+file.close()
