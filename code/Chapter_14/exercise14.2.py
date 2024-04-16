87e98a488bd1fd6cc2c01e8840c0928d062135f5
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-16 13:27:58 +0200

    to split pdf in 2 pieces

--- code/Chapter_14/exercise14.2.py
+++ code/Chapter_14/exercise14.2.py
@@ -13,3 +13,27 @@ pdf_writer.addPage()
 output_path = Path.home() / "last_page.pdf"
 with output_path.open(mode="wb") as output_file:
     pdf_writer.write(output_file)
+    
+#need to do number 2
+
+
+first_half_writer = PdfWriter
+second_half_writer = PdfWriter
+
+first_half_pages = Pdf_reader.pages[0:150]
+second_half_pages = Pdf_reader.pages[150:]
+
+for page in first_half_pages:
+    first_half_writer.addPage(page)
+    
+for page in second_half_pages:
+    second_half_writer.addPage(page)
+
+first_half_output_path = Path.home() / "first_half.pdf"
+with first_half_output_path.open(mode="wb") as output_file:
+    first_half_writer.write(first_half_output_path)
+    
+
+second_half_output_path = Path.home() / "second_half.pdf"
+with second_half_output_path.open(mode="wb") as output_file:
+    second_half_writer.write(second_half_output_path)
