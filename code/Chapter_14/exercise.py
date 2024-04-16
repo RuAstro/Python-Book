aee2dc30b059082e11c29a1f9570d9c6f3fe6dc4
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-16 09:43:42 +0200

    fixed the pdf reader

--- code/Chapter_14/exercise.py
+++ code/Chapter_14/exercise.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-from PyPDF2 import pdfFileReader
+from PyPDF2 import PdfReader
+pdf = Path.cwd()
+pdf = Path.cwd() / "code" / "Chapter_14" / "zen.pdf"
 
-pdf = Path.cwd() / "user" / "zen.pdf"
-
-pdf_reader = pdfFileReader(str(pdf))
+pdf_reader = PdfReader(str(pdf))
 
 num_pages = pdf_reader.getNumPages()
 print(num_pages)
