df6b26604015a3faf7f1cfd7b52f3ae9cf703e7f
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-24 14:21:31 +0200

    added 3 values to db, remove db with os

--- code/Chapter_14/Chapter_15/exercise.py
+++ code/Chapter_14/Chapter_15/exercise.py
@@ -1,31 +1,49 @@
 import sqlite3
+from pathlib import Path
+import os
+
+database = "roster.db"
+os.remove(database)
 
 # Creating db and table.
-connection = sqlite3.connect("roster.db")
-cursor = connection.cursor()
-cursor.execute(
-    """CREATE TABLE IF NOT EXISTS ROSTER (
-               Name TEXT,
-               Species TEXT,
-               Age,
-               )"""
-)
+create_table = """
+CREATE TABLE People(
+    FirstName Text,
+    SpeciesName Text,
+    Age INT
+);"""
+# Add data
+insert_values1 = """
+INSERT INTO People VALUES(
+    'Benjamin',
+    'Sisko',
+    40
+);"""
 
-connection.commit()
-connection.close()
+insert_values2 = """
+INSERT INTO People VALUES(
+    'Jadzia-Dax',
+    'Trill',
+    300
+);"""
 
-# Adding data
-data = (
-    ("Benjamin Sisko", "Human", 40),
-    ("Jadzia Dax", "Trill", 300),
-    ("Kira Nerys", "Bajordan", 29),
-)
+insert_values3 = """
+INSERT INTO People VALUES(
+    'Kira-Nerys',
+    'bajordan',
+    29
+);"""
 
-cursor.executemany("Insert Into Roster VALUES(?, ?, ?)", data)
+with sqlite3.connect(database) as connection:
+    cursor = connection.cursor()
+    cursor.execute(create_table)
+    cursor.execute(insert_values1)
+    cursor.execute(insert_values2)
+    cursor.execute(insert_values3)
 
 # Update method
-cursor.execute("UPDATE data SET Name=?", ("Ezri dax", "Jadiza Dax"))
+# cursor.execute("UPDATE data SET Name=?", ("Ezri dax", "Jadiza Dax"))
 
-cursor.execute("Select Name, Where Species='Barjoran', Age ")
-for row in cursor.fetchall():
-    print(row)
+# cursor.execute("Select Name, Where Species='Barjoran', Age ")
+# for row in cursor.fetchall():
+#     print(row)
