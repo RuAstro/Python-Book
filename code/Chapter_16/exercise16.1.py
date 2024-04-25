b07a40554201cc0f043744b9989408d43db0f3e6
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-04-25 19:53:58 +0200

    commit

--- code/Chapter_16/exercise16.1.py
+++ code/Chapter_16/exercise16.1.py
@@ -5,5 +5,6 @@ page = urlopen(url)
 page
 
 for tag in ["Name: ", "Fav Color: "]:
-    tag = html[start_index : end - index]
-    tag
+    tag_start = html_text.find(tag) + len(tag)
+    tag_end = html_text[tag_start:].find("<")
+    print(tag)
