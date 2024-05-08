8b81c22dea2dc996be34de7680d3a0d555211f22
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-05-08 21:20:31 +0200

    diffrent widgets

---
+++ code/Chapter_18/working_with_widgets.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+# label Widgets
+# label = tk.Label(text="Hello, Tkinter", fg="white", bg="#34A2FE", width=10, height=10)
+# label.pack()
+
+# Button Widgets
+# button = tk.Button(text="Click me!", width=25, height=5, bg="blue", fg="yellow")
+# button.pack()
+
+
+# Entry Widgets
+# entry = tk.Entry(fg="yellow", bg="blue", width=50)
+# entry.pack()
+
+# label = tk.Label(text="Name")
+# entry = tk.Entry()
+
+# label.pack()
+# entry.pack()
+
+# name = entry.get()
+# print(name)
+
+# entry.delete(0, tk.END)
+# entry.insert(0, "Real ")
+
+# tEXT WIGETS
+window = tk.Tk()
+text_box = tk.Text()
+text_box.get("1, 0")
+text_box.pack()
+
+
+window.mainloop()
