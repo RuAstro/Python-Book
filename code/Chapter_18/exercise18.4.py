b102be0cf7bee4e50e25411cc511664fd8ff8fae
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-05-08 21:17:53 +0200

    adding text to labels

---
+++ code/Chapter_18/exercise18.4.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+window = tk.Tk()
+# label = tk.Label(text="GUIs are great!")
+# label.pack()
+# window.mainloop()
+
+# label = tk.Label(text="Python rocks!")
+# label.pack()
+# window.mainloop()
+
+label = tk.Label(text="Engage!")
+label.pack()
+window.mainloop()
