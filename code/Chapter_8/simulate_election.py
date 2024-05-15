c2845c06dbc396603eb79ab67b41a742b43e11d8
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-05-15 15:54:00 +0200

    used += operator for incrementing variables, to not  say a + b + 1 hole time, shorten code and works the same

--- code/Chapter_8/simulate_election.py
+++ code/Chapter_8/simulate_election.py
@@ -4,33 +4,38 @@ num_times_A_wins = 0
 num_times_B_wins = 0
 
 num_trails = 10_000
-for trail in range(0, num_trails):
+
+for trail in range(num_trails):
     votes_for_A = 0
     votes_for_B = 0
-   #First regions 
+# First regions
 if random() < 0.87:
-    votes_for_A = votes_for_A + 1
+    votes_for_A += 1
 else:
-    votes_for_B = votes_for_B + 1
-    #Second regions 
+    votes_for_B += 1
+    # Second regions
 if random() < 0.65:
-    votes_for_A = votes_for_A + 1
+    votes_for_A += 1
 else:
-    votes_for_B = votes_for_B + 1
-    #Third regions 
+    votes_for_B += 1
+    # Third regions
 if random() < 0.17:
-    votes_for_A = votes_for_A + 1
+    votes_for_A += 1
 else:
-    votes_for_B = votes_for_B + 1
-    
+    votes_for_B += 1
 
-#outcome...
+
+# outcome...
 if votes_for_A > votes_for_B:
-    num_times_A_wins = num_times_A_wins + 1
-    
+    num_times_A_wins += 1
+
 else:
-    num_times_B_wins = num_times_B_wins + 1
-    
-    
-print(f"Probability A wins: {num_times_A_wins / num_trails}")
-print(f"Probability B wins: {num_times_B_wins / num_trails}")    
+    num_times_B_wins += 1
+
+
+prob_A_wins = num_times_A_wins / num_trails
+prob_B_wins = num_times_B_wins / num_trails
+
+
+print(f"Probability A wins: {prob_A_wins}")
+print(f"Probability B wins: {prob_B_wins}")
